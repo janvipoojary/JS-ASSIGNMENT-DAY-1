ea1a19fb0d8831bf8bc2f853d730aa756f75dbb9
janvipoojary <68179563+janvipoojary@users.noreply.github.com>
2020-07-15 13:56:06 +0530

    Add files via upload

---
+++ Assignment3.js
@@ -0,0 +1,23 @@
+var length = 16;
+// number
+
+var lastName = "Johnson";
+// String
+
+var a = null;
+console.log(a);   // This returns null
+
+var a;
+console.log(a); // This will return undefined.
+
+var a = 5;
+var b = 6;
+a == b // returns false
+
+var x = { firstName: "John", lastName: "Doe" };
+// Object
+
+var arr1 = [1, 2, 3];
+console.log(arr1) // This will return the array [
+
+function a() { }
