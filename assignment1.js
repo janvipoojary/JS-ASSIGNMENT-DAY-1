7aa99388f92d0d0ef8bfdb12174159e54baec4e6
janvipoojary <68179563+janvipoojary@users.noreply.github.com>
2020-07-15 13:15:13 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,33 @@
+console.log();
+// this helps to print the output on console.
+
+console.error();
+// this helps to find out error
+
+console.warn();
+// used to log warning message on console.
+
+console.time();
+console.timeEnd();
+// whenever we want to know the amount of time spend by a block or function.
+
+console.table();
+// this method allows us to generate a table inside console.The input must be  an array or object which will be shown on table.
+
+console.clear();
+// used to clear the console.
+
+console.count();
+// this is used  to count the number that the function hit by this counting method.
+
+console.group();
+console.groupEnd()
+// this allows us to group contents in a separate block, which will be indented.
+
+/*custom console logs
+here user can add styling to the console  logs in order to makes logs custom
+example:
+
+const spacing = "10px";
+const styles
+etd*/
